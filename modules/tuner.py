3990474c3b21026b3e39099d4976fb6c3f027c3d
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-09-13 23:15:46 +0700

    applied clean code on tuner module

--- modules/tuner.py
+++ modules/tuner.py
@@ -3,10 +3,11 @@
 # pylint: disable=duplicate-code
 
 from typing import Any, Dict, NamedTuple, Text
+import keras
+from keras import layers
 import keras_tuner as kt
 import tensorflow as tf
 import tensorflow_transform as tft
-import tf_keras as keras
 from tfx.components.trainer.fn_args_utils import FnArgs
 
 LABEL_KEY = 'price'
@@ -51,54 +52,47 @@ def model_builder(hp: kt.HyperParameters) -> keras.Model:
     """Membangun arsitektur model Keras untuk pencarian hyperparameter."""
     inputs = {}
 
-    # Layer input numerik
     for feat in NUMERICAL_FEATURES:
-        inputs[transformed_name(feat)] = keras.layers.Input(
+        inputs[transformed_name(feat)] = layers.Input(
             shape=(1,), name=transformed_name(feat), dtype=tf.float32
         )
 
-    # Layer input kategorikal
     for feat in CATEGORICAL_FEATURES:
-        inputs[transformed_name(feat)] = keras.layers.Input(
+        inputs[transformed_name(feat)] = layers.Input(
             shape=(1,), name=transformed_name(feat), dtype=tf.int64
         )
 
-    # Konversi categorical index ke embedding representation dengan index clipping
     cat_embeddings = []
     for feat in CATEGORICAL_FEATURES:
-        cleaned = keras.layers.Lambda(
+        cleaned = layers.Lambda(
             lambda x: tf.clip_by_value(
                 x, tf.cast(0, x.dtype), tf.cast(149, x.dtype)
             )
         )(inputs[transformed_name(feat)])
-        embed = keras.layers.Embedding(input_dim=150, output_dim=8)(cleaned)
-        cat_embeddings.append(keras.layers.Flatten()(embed))
+        embed = layers.Embedding(input_dim=150, output_dim=8)(cleaned)
+        cat_embeddings.append(layers.Flatten()(embed))
 
     num_layers = [inputs[transformed_name(f)] for f in NUMERICAL_FEATURES]
-    all_features = keras.layers.concatenate(num_layers + cat_embeddings)
+    all_features = layers.concatenate(num_layers + cat_embeddings)
 
-    # Tuning jumlah unit layer dense pertama
     units_1 = hp.Int(
         'units_1', min_value=32, max_value=128, step=32, default=64
     )
-    layer_x = keras.layers.Dense(units_1, activation='relu')(all_features)
+    layer_x = layers.Dense(units_1, activation='relu')(all_features)
 
-    # Tuning dropout
     dropout_rate = hp.Float(
         'dropout_rate', min_value=0.1, max_value=0.4, step=0.1, default=0.2
     )
-    layer_x = keras.layers.Dropout(dropout_rate)(layer_x)
+    layer_x = layers.Dropout(dropout_rate)(layer_x)
 
-    # Tuning jumlah unit layer dense kedua
     units_2 = hp.Int(
         'units_2', min_value=16, max_value=64, step=16, default=32
     )
-    layer_x = keras.layers.Dense(units_2, activation='relu')(layer_x)
-    output = keras.layers.Dense(1, activation='linear')(layer_x)
+    layer_x = layers.Dense(units_2, activation='relu')(layer_x)
+    output = layers.Dense(1, activation='linear')(layer_x)
 
     model = keras.Model(inputs=inputs, outputs=output)
 
-    # Tuning learning rate optimizer
     learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 5e-4])
     model.compile(
         optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
